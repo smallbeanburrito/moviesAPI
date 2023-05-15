1b6aa2d27d3aa6852b6389287f8bbc274611aa42
Keith <kcao13@myseneca.ca>
2023-05-15 13:23:38 -0400

    add routes

--- server.js
+++ server.js
@@ -15,6 +15,36 @@ app.get("/", (req,res) => {
     res.send({message:"API Listening"});
 });
 
+app.post("/api/movies", (req,res) => {
+    db.addNewMovie(req.body)
+    .then((data) => res.status(201).send(data))
+    .catch(() => res.status(500).send("error creating movie"));
+});
+
+app.get("/api/movies", (req,res) => {
+    db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send("error retrieving movies"));
+});
+
+app.get("/api/movies/:id", (req,res) => {
+    db.getMovieById(req.params.id)
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send("error retrieving movie"));
+});
+
+app.put("/api/movies/:id", (req,res) => {
+    db.updateMovieById(req.params.id, req.body)
+    .then((data) => res.status(204).send(data))
+    .catch(() => res.status(500).send("error updating movie"));
+});
+
+app.delete("/api/movies/:id", (req,res) => {
+    db.deleteMovieById(req.params.id)
+    .then((data) => res.status(204).send(data))
+    .catch(() => res.status(500).send("error deleting movie"));
+});
+
 db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
     app.listen(HTTP_PORT, ()=>{
         console.log(`server listening on: ${HTTP_PORT}`);
