6f3a9cf10d936067b475dd8a2406e0d8f047fb3a
Keith <kcao13@myseneca.ca>
2023-05-15 00:42:55 -0400

    initialization of mongodb

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ cors = require('cors');
 app.use(cors());
 require('dotenv').config();
 
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
+
 app.use(express.json());
 
 HTTP_PORT = process.env.PORT || 8080;
@@ -12,4 +15,10 @@ app.get("/", (req,res) => {
     res.send({message:"API Listening"});
 });
 
-app.listen(HTTP_PORT);
+db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
+    app.listen(HTTP_PORT, ()=>{
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err)=>{
+    console.log(err);
+});
